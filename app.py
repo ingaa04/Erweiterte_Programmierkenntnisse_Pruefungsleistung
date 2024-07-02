7a7cf639b56e81dd6b50f5364864a470bafa7494
ingaa04 <168195256+ingaa04@users.noreply.github.com>
2024-07-02 22:06:05 +0200

    update1 of app.py

--- app.py
+++ app.py
@@ -4,50 +4,30 @@ import math
 
 app = Flask(__name__)
 
-# Lieder werden aus der JSON-file geladen
 def load_songs():
     with open('songs.json', 'r') as file:
         return json.load(file)
 
-
-# Lieder werden in der JSON-file gespeichert
 def save_songs(songs):
     with open('songs.json', 'w') as file:
         json.dump(songs, file)
 
-
-# Berechnunf der Indices der Songs (je nach Seite, auf der sie sich befinden)
-def get_paginated_songs(page=0, per_page=3, query=None):
-    songs = load_songs()
-    if query:
-        songs = [song for song in songs if
-                 query.lower() in song['title'].lower() or query.lower() in song['artist'].lower()]
-
-    start = page * per_page # Ermittlung der Indices der Songs für die jeweiligen "Seiten" --> Anpsassung über per_page einfach möglich
-    end = start + per_page
-    return songs[start:end]
-
-
-# Berechnung der aktuellen Anzahl an Seitenzahlen
-def get_total_pages(per_page=3, query=None):
-    songs = load_songs()
-    if query:
-        songs = [song for song in songs if
-                 query.lower() in song['title'].lower() or query.lower() in song['artist'].lower()]
-    total_pages = math.ceil(len(songs) / per_page)
-    return total_pages
-
-
 @app.route('/')
 def index():
+    songs = load_songs()
     query = request.args.get('query')
-    page = request.args.get('page', 0, type=int)
+    if query:
+        songs = [song for song in songs if query.lower() in song['title'].lower() or query.lower() in song['artist'].lower()]
+
+    # Pagination
+    page = request.args.get('page', 1, type=int)
     per_page = 3
-    total_pages = get_total_pages(per_page, query)
-    songs_paginated = get_paginated_songs(page, per_page, query)
+    total_pages = math.ceil(len(songs) / per_page)
+    start = (page - 1) * per_page
+    end = start + per_page
+    songs_paginated = songs[start:end]
 
     return render_template('index.html', songs=songs_paginated, page=page, total_pages=total_pages, query=query)
-
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_song():
@@ -63,7 +43,6 @@ def add_song():
         save_songs(songs)
         return redirect(url_for('index'))
     return render_template('add_song.html')
-
 
 @app.route('/edit/<int:song_id>', methods=['GET', 'POST'])
 def edit_song(song_id):
@@ -82,7 +61,6 @@ def edit_song(song_id):
     song = songs[song_id]
     return render_template('edit_song.html', song=song, song_id=song_id)
 
-
 @app.route('/delete/<int:song_id>', methods=['POST'])
 def delete_song(song_id):
     songs = load_songs()
@@ -91,7 +69,6 @@ def delete_song(song_id):
     del songs[song_id]
     save_songs(songs)
     return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
